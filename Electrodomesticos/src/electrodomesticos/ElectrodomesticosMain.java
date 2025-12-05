3bbb3049cdfe3780ef0ed75ff058e859db55b30f
alecito_calita <106605430+alejandrocalar@users.noreply.github.com>
2025-12-05 11:02:34 -0500

    Add files via upload

--- Electrodomesticos/src/electrodomesticos/ElectrodomesticosMain.java
+++ Electrodomesticos/src/electrodomesticos/ElectrodomesticosMain.java
@@ -9,5 +9,15 @@ package electrodomesticos;
  * @author SALON206
  */
 public class ElectrodomesticosMain {
-    
+    public static void main(String[] args) {
+        Electrodomesticos[] electrodomesticos = new Electrodomesticos[4];
+        electrodomesticos[0] = new Lavadora();
+        electrodomesticos[1] = new Lavadora(150, 10);
+        electrodomesticos[2] = new Television();
+        
+        for(Electrodomesticos e : electrodomesticos){
+            System.out.println("Electrodomestico: "+e.getConsumoEnergetico());
+        }
+    }
 }
+ 
